7f37279c0eba8890155a149e30dbc720357ede99
岩枭 <yanxiao@orbbec.com>
2025-08-19 13:50:15 +0800

    [kernel][feat]{commit AGX_Orin_DevKit/6.2/agx_orin_devkit_6.2_v1.2.06.patch}

--- src/patch/NVIDIA/AGX_Orin_DevKit/6.2/copy_to_jetson_ssh.sh
+++ src/patch/NVIDIA/AGX_Orin_DevKit/6.2/copy_to_jetson_ssh.sh
@@ -9,6 +9,7 @@ cp -r images/6.2/rootfs/boot/tegra234-p3737-camera-g300-nomtd-fg96-overlay.dtbo 
 cp -r images/6.2/rootfs/boot/tegra234-p3737-camera-g300-leopard-overlay.dtbo gmsl-driver-jetson
 cp -r images/6.2/rootfs/boot/tegra234-p3767-camera-p3768-g300-fg96-overlay.dtbo gmsl-driver-jetson
 cp -r images/6.2/rootfs/boot/tegra234-p3737-camera-g300-mic-fg-8g-overlay.dtbo gmsl-driver-jetson
+cp -r images/6.2/rootfs/boot/tegra234-p3737-camera-g300-nomtd-leopard-overlay.dtbo gmsl-driver-jetson
 
 
 cp images/6.2/rootfs/lib/modules/5.15.148-tegra/updates/drivers/media/platform/tegra/camera/tegra-camera.ko gmsl-driver-jetson
@@ -27,6 +28,7 @@ cp copy_to_target_agx_orin_nomtd_fg96.sh gmsl-driver-jetson
 cp copy_to_target_agx_orin_leopard.sh gmsl-driver-jetson
 cp copy_to_target_orin_nx_fg96.sh gmsl-driver-jetson
 cp copy_to_target_agx_orin_mic_fg_8g.sh gmsl-driver-jetson
+cp copy_to_target_agx_orin_nomtd_leopard.sh gmsl-driver-jetson
 cp reconnect.sh gmsl-driver-jetson
 
 scp -r gmsl-driver-jetson orbbec@xx.x.xxx.xxx:~/
